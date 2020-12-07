e916cfe4770d5834f1333a81f057455766ef78e0
AleksandrSekker <sekkerpleksandr@gmail.com>
2020-12-07 11:39:27 +0200

    Add second version for exersise1

--- lesson4/exercise1.py
+++ lesson4/exercise1.py
@@ -1,6 +1,12 @@
+import math
+
 input_number=int(input("Enter a number "))
+
+# first version
 result=1
-for i in range(1, input_number +1):
-    if i < input_number :
-        result=result*(input_number +1)
+for i in range(1, int(input_number) +1):
+        result=result*i
 print("Factorial of {} is {}".format(input_number, result))
+
+# second version
+print(math.factorial(int(input_number)))
