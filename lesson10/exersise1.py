3bd34b7d0683b5e97e5ebe8d2b044127e7e711b9
AleksandrSekker <sekkerpleksandr@gmail.com>
2020-12-22 18:15:44 +0200

    change

--- lesson10/exersise1.py
+++ lesson10/exersise1.py
@@ -1,19 +1,23 @@
 def check_odd_even(number):
     # n = int(input("Input entire number: "))
-    while type(number) != int:
+    while type(number) != int or number < 0:
         try:
             number = int(number)
+            if number <= 0:
+                raise ValueError('Cant be less than 0')
+            break
         except ValueError as e:
             print(e)
             number = input("Input entire number:\n ")
         except KeyboardInterrupt as e:
             print(e)
+        
     if number % 2 == 0:
         print("Entered number is even")
     else:
         print("Entered number is odd")
 
 check_odd_even(15)
-# cheсk_odd_even(-6)
-check_odd_even("gth")
+check_odd_even(-6)
+# check_odd_even("gth")
 # cheсk_odd_even(0)
